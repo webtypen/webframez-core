68a6384fdaec9e27a23718dd709498e835caac60
Benedict Romp <benedict.romp@googlemail.com>
2023-01-26 14:30:41 +0100

    Bugfix

--- src/ConsoleApplication.ts
+++ src/ConsoleApplication.ts
@@ -3,36 +3,54 @@ import { Config } from "./Config";
 import { BuildFinishCommand } from "./Commands/BuildFinishCommand";
 
 export class ConsoleApplication {
-    /**
-     * Init the routes and start the http-server
-     */
-    boot(options?: any) {
-        if (options && options.config) {
-            for (let key in options.config) {
-                Config.register(key, options.config[key]);
-            }
-        }
-
-        const program = new Command();
-        program.name("webframez console").description("CLI to some JavaScript string utilities").version("0.0.1");
-
-        const systemCommands = [BuildFinishCommand];
-        for (let command of systemCommands) {
-            if (!command.signature) {
-                continue;
-            }
-
-            const consoleCommand = new Command(command.signature);
-            if (command.description && command.description.trim() !== "") {
-                consoleCommand.description(command.description);
-            }
-            consoleCommand.action(async () => {
-                const service = await new command();
-                return await service.handle();
-            });
-            program.addCommand(consoleCommand);
-        }
-
-        program.parse();
+  /**
+   * Init the routes and start the http-server
+   */
+  boot(options?: any) {
+    if (options && options.config) {
+      for (let key in options.config) {
+        Config.register(key, options.config[key]);
+      }
     }
+
+    const program = new Command();
+    program.name("webframez console").description("CLI to some JavaScript string utilities").version("0.0.1");
+
+    const systemCommands = [BuildFinishCommand];
+    for (let command of systemCommands) {
+      if (!command.signature) {
+        continue;
+      }
+
+      const consoleCommand = new Command(command.signature);
+      if (command.description && command.description.trim() !== "") {
+        consoleCommand.description(command.description);
+      }
+      consoleCommand.action(async () => {
+        const service = await new command();
+        return await service.handle();
+      });
+      program.addCommand(consoleCommand);
+    }
+
+    if (options && options.kernel && options.kernel.commands && options.kernel.commands.length > 0) {
+      for (let command of options.kernel.commands) {
+        if (!command.signature) {
+          continue;
+        }
+
+        const consoleCommand = new Command(command.signature);
+        if (command.description && command.description.trim() !== "") {
+          consoleCommand.description(command.description);
+        }
+        consoleCommand.action(async () => {
+          const service = await new command();
+          return await service.handle();
+        });
+        program.addCommand(consoleCommand);
+      }
+    }
+
+    program.parse();
+  }
 }
